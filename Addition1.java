745b4ce8d70a670692d95afca903779b12e61cf6
Sushmitha <sushmithagajam@gmail.com>
2026-09-04 21:10:31 +0530

    Added Addition1.java program

---
+++ Addition1.java
@@ -0,0 +1,9 @@
+public class Addition1 {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+        int sum = a + b;
+
+        System.out.println("Sum is: " + sum);
+    }
+}
